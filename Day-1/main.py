00b0ea03e2cdd2d9c1f870d3772dc4d4bebd851c
mhamza-ali <hamza4841333@gmail.com>
2021-10-27 13:05:01 +0500

    project band name generator
    day 1 done with exercise practice and project

---
+++ Day-1/main.py
@@ -0,0 +1,49 @@
+print("Hello World!")
+
+#interactive exercise 1
+print("Day 1 - Python print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#print multiple line with single print statement
+print("Hello world!\nHello world!\nHello world!")
+
+#concateation
+print("Hello" + " " + "Hamza")
+
+#interactive exercise 2
+#fix the code
+print("Day 1 - String Manipulation")
+print("String concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world)')
+
+#input function
+print("Hello " + input("What is your name?") + "!")
+
+#input exercise
+#write program that counts the number of characters in a user's name
+s = input("What is your name? ")
+#length function
+print(len(s))
+print( len(input("Whats your name?")))
+
+#variables
+name = input("Whats your name?")
+print(name)
+length = len(name)
+print(length)
+
+#variables exercise
+# write a program which switches the values stored in variables
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+print("a = " + a)
+print("b = " + b)
+
+#variables naming
+user_name  = input("whats your fav programming language")
+print(user_name)
+
---
+++ project-1.py
@@ -0,0 +1,14 @@
+#1. Create a greeting for your program.
+print("Welcome to the band name generator.")
+
+#2. Ask the user for the city thatthey grew up in.
+city = input("Which city did you grow up in?\n")
+print(city)
+
+#3. Ask the user for the name of pet
+pet = input("What is the name of the pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name
+print("Your band name could be " + city + " " + pet)
+
+#5. Mkae sure the input cursor shows on a new line 
